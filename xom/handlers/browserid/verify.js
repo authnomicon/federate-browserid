c1d68adfe339457ed925a9176fe852d4a152dabe
Jared Hanson <jaredhanson@gmail.com>
2017-01-07 16:32:22 +1300

    Require ceremony by new interface name.

--- xom/handlers/browserid/verify.js
+++ xom/handlers/browserid/verify.js
@@ -29,7 +29,7 @@ exports = module.exports = function(resume, ceremony, store, authenticator) {
 
 exports['@require'] = [
   '../../ceremony/resume',
-  'http://i.bixbyjs.org/http/ua/flows/Dispatcher',
-  'http://i.bixbyjs.org/http/ua/flows/StateStore',
+  'http://i.bixbyjs.org/www/ceremony/Dispatcher',
+  'http://i.bixbyjs.org/www/ceremony/StateStore',
   'http://i.bixbyjs.org/http/Authenticator'
 ];
